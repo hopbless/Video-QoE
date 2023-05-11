89cd43518d7f0ace637e3b79536f6e9508a69a02
hopbless <hopeinyang@gmail.com>
2023-05-11 05:21:14 +0100

    added urlll

--- measurement.py
+++ measurement.py
@@ -49,7 +49,7 @@ def run_video():
 	try:
 		print(time.time(), ' start chrome')
 		s = Service(ChromeDriverManager().install())
-		browser = webdriver.Chrome(ChromeDriverManager().install())
+		browser = webdriver.Chrome(ChromeDriverManager().install(), desired_capabilities=caps)
 		#browser = webdriver.Chrome(desired_capabilities=caps)
 		browser.maximize_window()
 		time.sleep(10)
