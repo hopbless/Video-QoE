5a55c1abd0cfdd1301f43ba6a951a0518e7611f5
hopbless <hopeinyang@gmail.com>
2023-05-11 09:37:27 +0100

    update video file

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import numpy as np
 import flask
 from flask import Flask, render_template, request, redirect
 from measurement import run_video
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
 
 
 
@@ -9,9 +11,11 @@ from measurement import run_video
 app = Flask(__name__, template_folder='templates')
 
 
-@app.route("/")
+@app.route("/", methods=["GET","POST"])
 def home():
-    return render_template("index.html")
+    result = "Hope"
+
+    return render_template("index.html", hope=result)
 
 
 
@@ -20,10 +24,28 @@ def home():
 def play():
     return render_template("video_player.html")
 
+
 @app.route("/measurement", methods =["GET", "POST"])
 def record_data():
     run_video()
     return render_template("about.html")
+
+@app.route("/about", methods =["POST", "GET"])
+def about_page():
+    render_template("about.html", hope = "Result")
+
+
+def predictQoE():
+    
+    df = pd.read_csv("data/training_data.csv", header=0)
+    X = df.loc[:, df.columns != "score"]
+    type(X)
+    y = df["score"]
+    type(y)
+    scaler = StandardScaler()
+    x_std = scaler.fit_transform(X)
+    y_pred = svr_model.predict(X_train)
+
 
 
 
